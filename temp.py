9b094a9c99535ae2e88c32263d57f523c956c691
Charles Vann Aquino <aquinocharles19@yahoo.com>
2023-10-19 16:04:53 -0600

    Step 3 - WIP

--- temp.py
+++ temp.py
@@ -3,7 +3,16 @@ def celsius_to_fahrenheit(celsius):
     fah = celsius * 9/5 + 32
     return fah
 
+def fahrenheit_to_celsius(fahrenheit):
+    """ convert value given to celsius"""
+    celsius = fahrenheit - 32
+    return celsius
+
+
 # Program to convert Celsius to Fahrenheit
 celsius = 25
 fah=celsius_to_fahrenheit(celsius)
 print(f"{celsius} degree is {fah} in Fahrenheit")
+
+celsius=fahrenheit_to_celsius(fah)
+print(f"{fah} degree Fahrenheit is {celsius} in Celsius")
